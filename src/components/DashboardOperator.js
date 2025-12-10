626372bda7eb4163ff3f1dc7c1aacac8a790f773
Gampalavinuthna <vinuthnagampala@gmail.com>
2025-12-10 19:43:10 +0530

    Add files via upload

---
+++ src/components/DashboardOperator.js
@@ -0,0 +1,8 @@
+export default function DashboardOperator() {
+  return (
+    <div className="card">
+      <h2>Operator Dashboard</h2>
+      <p>Live Ship Tracking, Alerts, Real-Time Updates</p>
+    </div>
+  );
+}
